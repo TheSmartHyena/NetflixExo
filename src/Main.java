304f68725e78a2aa36a43ee24b9fdcfc53097099
Philippe SKOPAL <pskopal@nevaone.int>
2025-02-02 15:16:23 +0100

    Fake data in

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // We initialize our fake database with some data
+        Db dao = SDb.getInstance();
+        dao.create(FMovie.createMovie("The Texas Chain Saw Massacre", "83 minutes", EMovieCategory.HORROR));
+        dao.create(FMovie.createMovie("Interstellar", "169 minutes", EMovieCategory.SCIFI));
+        dao.create(FMovie.createMovie("John Wick", "101 minutes", EMovieCategory.ACTION));
+
+        
     }
 }
